999ca12df18480df04033d3ed289e8190c9aea1f
Dronowar <dronowar@gmail.com>
2015-05-05 22:53:32 +0300

    wellcome mail for new users

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -1,14 +1,15 @@
 <?php namespace App\Repositories;
 
 use App\User;
-use Cache;
+use Cache, Mail;
 
 class UserRepository {
-
  
     public function findByUseremailOrCreate($userData)
     {	
+        //ищем пользователя с таким e-mail-ом
     	$user = User::whereEmail($userData->email)->first();
+        //если такого нет, создаем нового
     	if (empty($user)) {
 	    	$user = new User;
 	    	$user->email = $userData->email;
@@ -16,9 +17,15 @@ class UserRepository {
 	    	$user->photo = $userData->avatar;
 	    	$user->active = true;
 	    	$user->save();
-            //need to send mail here
+            //пользователь создан нужно его поприветсвовать на e-mail
+            Mail::queue('emails.wellcome', [
+                'name' => $user->name
+            ], function($message) use ($user)
+            {
+                $message->to($user->email, $user->name)->subject('Добро пожаловать в Onpopri!');
+            });
     	}
-
+        //возвращаем либо найденного пользователя либо свежесозданного
     	return $user;
     }
 
